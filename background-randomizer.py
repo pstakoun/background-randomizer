89f6e9f922479702969688f5aa664b536a6d464f
Peter Stakoun <peter@stakoun.com>
2017-03-10 17:55:46 -0500

    Add resolution filter

--- background-randomizer.py
+++ background-randomizer.py
@@ -4,6 +4,7 @@ import os
 import random
 import urllib.request
 from bs4 import BeautifulSoup
+from PIL import Image
 
 MAX_RECENT = 100
 ABS_PATH = os.path.dirname(os.path.abspath(__file__))
@@ -18,19 +19,21 @@ while flag:
 	while source.geturl() in recent:
 		source = urllib.request.urlopen(urllib.request.Request(random.choice(sources),data=None,headers={'User-Agent': 'background-randomizer'}))
 
-	print(source.geturl())
+		print(source.geturl())
 
-	recent.append(source.geturl())
+		open(os.path.join(ABS_PATH, 'recent.txt'), 'w').write('\n'.join(recent))
 
-	if len(recent) > MAX_RECENT:
-		del recent[0]
+		result = BeautifulSoup(source.read()).find_all('img', class_='preview')
 
-	open(os.path.join(ABS_PATH, 'recent.txt'), 'w').write('\n'.join(recent))
+		if len(result) > 0:
+			urllib.request.urlretrieve(result[0].get('src'), os.path.join(ABS_PATH, 'background-temp.jpg'))
+			im = Image.open(os.path.join(ABS_PATH, 'background-temp.jpg'))
+			res = [int(x) for x in os.popen('xrandr | grep "\*" | cut -d" " -f4').read().split('x')]
+			if im.width > res[0]/2 and im.height > res[1]/2:
+				recent.append(source.geturl())
+				if len(recent) > MAX_RECENT:
+					del recent[0]
+				flag = False
 
-	result = BeautifulSoup(source.read()).find_all('img', class_='preview')
-
-	if len(result) > 0:
-		urllib.request.urlretrieve(result[0].get('src'), os.path.join(ABS_PATH, 'background.jpg'))
-		flag = False
-
+os.rename(os.path.join(ABS_PATH, 'background-temp.jpg'), os.path.join(ABS_PATH, 'background.jpg'));
 os.system('gsettings set org.gnome.desktop.background picture-uri file://' + os.path.join(ABS_PATH, 'background.jpg'))
